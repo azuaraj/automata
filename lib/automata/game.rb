e68da592479c40987f43f96de5cd675e8a92439b
Antonio Azuara <antonioazuara94@gmail.com>
2020-11-09 16:24:13 -0600

    add test case for right simulation array

--- lib/automata/game.rb
+++ lib/automata/game.rb
@@ -3,10 +3,12 @@ module Automata
 
         attr_accessor :seed
         attr_accessor :generations
+        attr_accessor :current_arrangement_state
 
         def initialize(seed, generations)
             @seed = seed
             @generations = generations
+            @current_arrangement_state = []
         end
 
         def start
--- spec/automata_spec.rb
+++ spec/automata_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe Automata do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    expect(false).to eq(false)
+  end
+
+  it "returns correct simulation array" do
+    input = [[0,1,0],[0,0,1],[1,1,1],[0,0,0]] #Initial seed state
+    output = [[0,0,0],[1,0,1],[0,1,1],[0,1,0]] #expected output after 1 generation
+    game = Automata.game(input, 1)
+    game.start
+    expect(game.current_arrangement_state).to eq(input)
   end
 end
