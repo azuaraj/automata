4cd661feebda33e9ba0d86bb12270d2ecad71c06
Antonio Azuara <antonioazuara94@gmail.com>
2020-11-09 01:24:29 -0600

    initial commit

--- lib/automata.rb
+++ lib/automata.rb
@@ -1,6 +1,13 @@
 require "automata/version"
+require "automata/game"
 
 module Automata
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    class Error < StandardError; end
+
+    def game(seed, generations)
+      @game ||= Automata::Game.new seed, generations
+    end
+
+  end
 end
--- lib/automata/game.rb
+++ lib/automata/game.rb
@@ -1,5 +1,20 @@
 module Automata
     class Game
-        
+
+        attr_accessor :seed
+        attr_accessor :generations
+
+        def initialize(seed, generations)
+            @seed = seed
+            @generations = generations
+        end
+
+        def start
+        end
+
+        def stop
+        end 
+
+
     end
 end
